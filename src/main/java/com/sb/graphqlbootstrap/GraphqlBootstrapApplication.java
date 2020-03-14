75fd1e8230ec5aad4c75a1fe021f5463455f8d92
Andrea Bredice <bredice.andrea@gmail.com>
2020-03-14 11:27:37 +0100

    first commit for base project bootstrap

---
+++ src/main/java/com/sb/graphqlbootstrap/GraphqlBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.sb.graphqlbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/graphqlbootstrap/GraphqlBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.graphqlbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
